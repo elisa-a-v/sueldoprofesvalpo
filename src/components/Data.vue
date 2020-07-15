a3df61f2674265015b637bfa697b3b69612d70bf
glycaemia <elisa.anguita@gmail.com>
2020-07-15 18:52:48 -0400

    Agregué algo de texto a la única ventana visible (Data.vue)

---
+++ src/components/Data.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <br>Departamento de Educación - Comunal Valparaíso
+    <br>Colegio de Profesoras y Profesores
+    <div>
+      <h1>
+        <br>Calculadora de sueldo
+      </h1>
+      <h5>
+        Herramienta para profesoras y profesores que trabajan en
+        <br>establecimientos municipales de la comuna de Valparaíso.
+      </h5>
+
+    </div>
+    <div>
+      <h4>
+        <br>INSTRUCCIONES:
+      </h4>
+      A continuación ingrese la información que se solicita y presione
+      <br>el botón "Calcular sueldo", y verá el detalle por asignación que debería
+      <br>aparecer en su colilla de sueldo.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style scoped>
+
+</style>
